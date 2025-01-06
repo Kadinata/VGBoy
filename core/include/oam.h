17178bfc75eafa7b6d0596c931e1c83d6ac32707
Kadinata <2396847+Kadinata@users.noreply.github.com>
2025-01-06 01:26:45 -0800

    Adding OAM scan to the OAM module

--- core/include/oam.h
+++ core/include/oam.h
@@ -5,31 +5,82 @@
 #include "bus_interface.h"
 #include "status_code.h"
 
-#define OAM_ATTR_CGB_PALETTE_NUM (0x7)
-#define OAM_ATTR_CGB_VRAM_BANK (1 << 3)
-#define OAM_ATTR_DMG_PALETTE_NUM (1 << 4)
-#define OAM_ATTR_X_FLIP (1 << 5)
-#define OAM_ATTR_Y_FLIP (1 << 6)
-#define OAM_ATTR_BG_PRIORITY (1 << 7)
+#define OAM_ENTRY_SIZE (40)
+#define MAX_SPRITES_PER_LINE (10)
 
+/**
+ * OAM attribute bit field
+ */
+typedef enum
+{
+  OAM_ATTR_CGB_PALETTE_NUM = (0x7),    /** (CGB only) Indicates which of the OBP0-7 palette to use */
+  OAM_ATTR_CGB_VRAM_BANK = (1 << 3),   /** (CGB only) Indicates which VRAM bank (0 or 1) to fetch from */
+  OAM_ATTR_DMG_PALETTE_NUM = (1 << 4), /** (Non-CGB only) Indicates which of the OBP0-1 palette to use */
+  OAM_ATTR_X_FLIP = (1 << 5),          /** Indicates whether the sprite should be flipped horizontally */
+  OAM_ATTR_Y_FLIP = (1 << 6),          /** Indicates whether the sprite should be flipped vertically */
+  OAM_ATTR_BG_PRIORITY = (1 << 7),     /** Indicates whether the background or window should be drawn over the sprite instead */
+} oam_attr_t;
+
+typedef enum
+{
+  OBJ_SIZE_SMALL = 8,
+  OBJ_SIZE_LARGE = 16,
+} obj_size_t;
+
+/**
+ * OAM entry definition
+ */
 typedef struct
 {
-  uint8_t y_pos;
-  uint8_t x_pos;
-  uint8_t tile;
-  uint8_t attrs;
+  uint8_t y_pos; /** Object's (sprite's) vertical position on the screen + 16 */
+  uint8_t x_pos; /** Object's (sprite's) horizontal position on the screen + 8 */
+  uint8_t tile;  /** Object's (sprite's) tile index in VRAM */
+  uint8_t attrs; /** Object's (sprite's) attribute flags */
 } oam_entry_t;
 
+/**
+ * Structure to store sprites collected during an OAM scan
+ */
+typedef struct
+{
+  uint8_t sprite_count;                                /** Number of collected sprites after the scan */
+  oam_entry_t sprite_attributes[MAX_SPRITES_PER_LINE]; /** Copies of collected sprite data */
+} oam_scanned_sprites_t;
+
+/**
+ * Top-level OAM data structure definition
+ */
 typedef struct
 {
   union
   {
-    oam_entry_t entries[40];
-    uint8_t oam_buf[sizeof(oam_entry_t) * 40];
+    oam_entry_t entries[OAM_ENTRY_SIZE];                   /** OAM entry data store */
+    uint8_t oam_buf[sizeof(oam_entry_t) * OAM_ENTRY_SIZE]; /** OAM entry data store as byte array */
   };
-  bus_interface_t bus_interface;
+  bus_interface_t bus_interface; /** Interface to allow the data bus to write to and read from the OAM */
 } oam_handle_t;
 
-status_code_t oam_init(oam_handle_t* const oam_handle);
+/**
+ * Initializes the OAM and configures its bus interface
+ *
+ * @param oam_handle Pointer to the OAM object to initialize
+ *
+ * @return `STATUS_OK` if initialization is successful, otherwise appropriate error code.
+ */
+status_code_t oam_init(oam_handle_t *const oam_handle);
+
+/**
+ * Scan the OAM for up to sprites that intersects the provided scan line.
+ * This function is to be called during mode 2 (OAM scan) of PPU rendering.
+ * Sprites returned by this function are sorted by their X coordinate in ascending order.
+ *
+ * @param oam_handle Pointer to the OAM object to scan the sprites from
+ * @param line_y Current scanline y-coordinate, the same as the current value of the LCD register LY
+ * @param sprite_size Indicates whether the sprite is 8 pixels or 16 pixels tall, as determined by bit-2 of the LCDC register
+ * @param scan_results Pointer to a scan result object to store the scanned sprites.
+ *
+ * @return `STATUS_OK` if initialization is successful, otherwise appropriate error code.
+ */
+status_code_t oam_scan(oam_handle_t *const oam_handle, uint8_t const line_y, obj_size_t const sprite_size, oam_scanned_sprites_t *const scan_results);
 
 #endif /* __DMG_OAM_H__ */
--- core/src/oam.c
+++ core/src/oam.c
@@ -1,11 +1,17 @@
 #include "oam.h"
 
 #include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "bus_interface.h"
 #include "status_code.h"
 
 static status_code_t oam_read(void *const resource, uint16_t const address, uint8_t *const data);
 static status_code_t oam_write(void *const resource, uint16_t const address, uint8_t const data);
+static inline bool scanline_intersects_sprite(oam_entry_t *oam_entry, uint8_t line_y, obj_size_t obj_size);
+static int sort_compare(const void *value_1, const void *value_2);
 
 /**
  * TODO: Disable OAM read/write by the CPU during DMA transfer
@@ -20,6 +26,34 @@ status_code_t oam_init(oam_handle_t *const oam_handle)
   oam_handle->bus_interface.read = oam_read;
   oam_handle->bus_interface.write = oam_write;
   oam_handle->bus_interface.resource = oam_handle;
+  memset(oam_handle->entries, 0, sizeof(oam_handle->entries));
+
+  return STATUS_OK;
+}
+
+status_code_t oam_scan(oam_handle_t *const oam_handle, uint8_t const line_y, obj_size_t const sprite_size, oam_scanned_sprites_t *const scan_results)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(oam_handle);
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(scan_results);
+  VERIFY_COND_RETURN_STATUS_IF_TRUE((sprite_size != OBJ_SIZE_LARGE) && (sprite_size != OBJ_SIZE_SMALL), STATUS_ERR_INVALID_ARG);
+
+  memset(scan_results, 0, sizeof(oam_scanned_sprites_t));
+
+  uint8_t index = 0;
+
+  // Scan until the end of OAM is reached or 10 sprites have been collected
+  while ((index < OAM_ENTRY_SIZE) && (scan_results->sprite_count < MAX_SPRITES_PER_LINE))
+  {
+    oam_entry_t *const oam_entry = &oam_handle->entries[index++];
+
+    if (scanline_intersects_sprite(oam_entry, line_y, sprite_size))
+    {
+      scan_results->sprite_attributes[scan_results->sprite_count++] = *oam_entry;
+    }
+  }
+
+  // Sort the sprites based on their X coordinate in ascending order
+  qsort(scan_results->sprite_attributes, scan_results->sprite_count, sizeof(oam_entry_t), sort_compare);
 
   return STATUS_OK;
 }
@@ -46,4 +80,16 @@ static status_code_t oam_write(void *const resource, uint16_t const address, uin
   oam_handle->oam_buf[address] = data;
 
   return STATUS_OK;
+}
+
+static inline bool scanline_intersects_sprite(oam_entry_t *oam_entry, uint8_t line_y, obj_size_t sprite_size)
+{
+  return ((oam_entry->y_pos <= (line_y + 16)) && (oam_entry->y_pos + sprite_size) > (line_y + 16));
+}
+
+static int sort_compare(const void *value_1, const void *value_2)
+{
+  const oam_entry_t *entry_1 = (oam_entry_t *)value_1;
+  const oam_entry_t *entry_2 = (oam_entry_t *)value_2;
+  return (entry_1->x_pos > entry_2->x_pos) - (entry_1->x_pos < entry_2->x_pos);
 }
