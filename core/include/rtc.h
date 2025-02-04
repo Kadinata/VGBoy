835a7fb4a3a5227951e6d88200b2dfbf89d30118
Kadinata <2396847+Kadinata@users.noreply.github.com>
2025-02-03 21:55:32 -0800

    Adding the RTC module

---
+++ core/include/rtc.h
@@ -0,0 +1,61 @@
+#ifndef __DMG_RTC_H__
+#define __DMG_RTC_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include "status_code.h"
+
+typedef enum
+{
+  RTC_DAY_MSB = (1 << 0),
+  RTC_HALT = (1 << 6),
+  RTC_DAY_OVERFLOW = (1 << 7),
+} rtc_dctrl_mask_t;
+
+typedef enum
+{
+  RTC_REG_SECONDS = 0,
+  RTC_REG_MINUTES,
+  RTC_REG_HOURS,
+  RTC_REG_DAYS_L,
+  RTC_REG_DAY_CTRL,
+  RTC_REG_MAX,
+} rtc_reg_type_t;
+
+typedef union
+{
+  struct
+  {
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hours;
+    uint8_t days;
+    uint8_t dctrl;
+  };
+  uint8_t buffer[RTC_REG_MAX];
+} rtc_registers_t;
+
+typedef struct
+{
+  time_t current_timestamp;
+  time_t prev_timestamp;
+  rtc_reg_type_t active_reg;
+  bool prev_latch;
+} rtc_state_t;
+
+typedef struct
+{
+  rtc_registers_t registers;
+  rtc_state_t state;
+} rtc_handle_t;
+
+status_code_t rtc_init(rtc_handle_t *const rtc);
+status_code_t rtc_sync(rtc_handle_t *const rtc);
+status_code_t rtc_latch(rtc_handle_t *const rtc, bool latch);
+status_code_t rtc_read(rtc_handle_t *const rtc, uint8_t *const data);
+status_code_t rtc_write(rtc_handle_t *const rtc, uint8_t const data);
+status_code_t rtc_select_reg(rtc_handle_t *const rtc, rtc_reg_type_t const reg);
+
+#endif /* __DMG_RTC_H__ */
---
+++ core/src/rtc.c
@@ -0,0 +1,151 @@
+#include "rtc.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include "status_code.h"
+
+#define MAX_RTC_DAYS (0x1FF)
+#define MAX_RTC_VALUE ((MAX_RTC_DAYS + 1) * 60 * 60 * 24)
+
+static void rtc_halt(rtc_handle_t *const rtc, bool halt);
+static void rtc_sync_to_regs(rtc_handle_t *const rtc, rtc_registers_t *const regs);
+static inline void rtc_check_overflow(rtc_handle_t *const rtc);
+static inline void load_timestamp_to_regs(rtc_handle_t *const rtc, rtc_registers_t *const regs);
+static inline bool is_halted(rtc_handle_t *const rtc);
+
+static const uint8_t rtc_register_masks[] = {0x3F, 0x3F, 0x1F, 0xFF, 0xC1};
+
+status_code_t rtc_init(rtc_handle_t *const rtc)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+
+  rtc->state.current_timestamp = 0;
+  rtc->state.prev_timestamp = time(NULL);
+
+  return STATUS_OK;
+}
+
+status_code_t rtc_select_reg(rtc_handle_t *const rtc, rtc_reg_type_t const reg)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+  VERIFY_COND_RETURN_STATUS_IF_TRUE(reg >= sizeof(rtc_registers_t), STATUS_ERR_INVALID_ARG);
+
+  rtc->state.active_reg = reg;
+
+  return STATUS_OK;
+}
+
+status_code_t rtc_latch(rtc_handle_t *const rtc, bool latch)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+
+  status_code_t status = STATUS_OK;
+
+  if (!rtc->state.prev_latch && latch)
+  {
+    rtc_sync(rtc);
+    load_timestamp_to_regs(rtc, &rtc->registers);
+  }
+
+  rtc->state.prev_latch = latch;
+
+  return STATUS_OK;
+}
+
+status_code_t rtc_read(rtc_handle_t *const rtc, uint8_t *const data)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(data);
+
+  *data = rtc->registers.buffer[rtc->state.active_reg] & rtc_register_masks[rtc->state.active_reg];
+
+  return STATUS_OK;
+}
+
+status_code_t rtc_write(rtc_handle_t *const rtc, uint8_t const data)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+
+  status_code_t status = STATUS_OK;
+  rtc_registers_t mirror_regs = {0};
+
+  if (is_halted(rtc))
+  {
+    load_timestamp_to_regs(rtc, &mirror_regs);
+    mirror_regs.buffer[rtc->state.active_reg] = data & rtc_register_masks[rtc->state.active_reg];
+    rtc_sync_to_regs(rtc, &mirror_regs);
+  }
+
+  if (rtc->state.active_reg == RTC_REG_DAY_CTRL)
+  {
+    rtc_halt(rtc, !!(data & RTC_HALT));
+    rtc->registers.dctrl &= ~(RTC_DAY_OVERFLOW | RTC_HALT);
+    rtc->registers.dctrl |= (data & (RTC_DAY_OVERFLOW | RTC_HALT));
+  }
+
+  return STATUS_OK;
+}
+
+status_code_t rtc_sync(rtc_handle_t *const rtc)
+{
+  VERIFY_PTR_RETURN_ERROR_IF_NULL(rtc);
+  VERIFY_COND_RETURN_STATUS_IF_TRUE(is_halted(rtc), STATUS_OK);
+
+  time_t delta = time(NULL) - rtc->state.prev_timestamp;
+  rtc->state.current_timestamp += delta;
+  rtc->state.prev_timestamp += delta;
+
+  rtc_check_overflow(rtc);
+
+  return STATUS_OK;
+}
+
+static void rtc_halt(rtc_handle_t *const rtc, bool halt)
+{
+  if (is_halted(rtc) && !halt)
+  {
+    rtc->state.prev_timestamp = time(NULL);
+  }
+  else if (!is_halted(rtc) && halt)
+  {
+    rtc_sync(rtc);
+  }
+}
+
+static void rtc_sync_to_regs(rtc_handle_t *const rtc, rtc_registers_t *const regs)
+{
+  uint16_t total_days = ((regs->dctrl & RTC_DAY_MSB) << 8) | regs->days;
+  rtc->state.current_timestamp = 0;
+  rtc->state.current_timestamp += regs->seconds;
+  rtc->state.current_timestamp += regs->minutes * 60;
+  rtc->state.current_timestamp += regs->hours * 60 * 60;
+  rtc->state.current_timestamp += total_days * 60 * 60 * 24;
+
+  rtc_check_overflow(rtc);
+}
+
+static inline void rtc_check_overflow(rtc_handle_t *const rtc)
+{
+  if ((rtc->state.current_timestamp / 60 / 60 / 24) > MAX_RTC_DAYS)
+  {
+    rtc->state.current_timestamp %= MAX_RTC_VALUE;
+    rtc->registers.dctrl |= RTC_DAY_OVERFLOW;
+  }
+}
+
+static inline void load_timestamp_to_regs(rtc_handle_t *const rtc, rtc_registers_t *const regs)
+{
+  regs->seconds = rtc->state.current_timestamp % 60;
+  regs->minutes = (rtc->state.current_timestamp / 60) % 60;
+  regs->hours = (rtc->state.current_timestamp / 60 / 60) % 24;
+  regs->days = (rtc->state.current_timestamp / 60 / 60 / 24) & 0xFF;
+  regs->dctrl &= ~RTC_DAY_MSB;
+  regs->dctrl |= ((rtc->state.current_timestamp / 60 / 60 / 24) & 0x100) ? RTC_DAY_MSB : 0;
+}
+
+static inline bool is_halted(rtc_handle_t *const rtc)
+{
+  return !!(rtc->registers.dctrl & RTC_HALT);
+}
